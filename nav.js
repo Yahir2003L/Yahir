60bec85838f471b329e9a5bba9bce171e3af7255
Yahir2003 <lopezyahir884@gmail.com>
2023-03-29 16:17:18 -0600

    up date

--- nav.js
+++ nav.js
@@ -5,3 +5,21 @@ menu.onclick = function(){
 }
 
 
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header ul li a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if( top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('activo');
+                document.querySelector('header ul li a[href*=' + id + ']').classList.add('activo');
+            });
+        };
+    });
+};
